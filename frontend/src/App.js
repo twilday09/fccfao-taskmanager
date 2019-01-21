08cfa6c82a55215b8f66fc03255db5d66bd9cda3
Jorge Encarnação <jfgencarnacao@gmail.com>
2019-01-21 19:47:21 +0000

    Added the missing files. Kids... don't be like me...

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-3">
+            <p>Hello World!</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
